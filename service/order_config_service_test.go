e48e7398f1f430b64a843002c56a1f2e60280848
Eadweard <aurora.coder.zqf@gmail.com>
2025-10-27 11:33:32 +0800

    feat: 新增配置

--- service/order_config_service_test.go
+++ service/order_config_service_test.go
@@ -16,5 +16,22 @@ func TestSaveOrderConfig(t *testing.T) {
 	if err != nil {
 		t.Fatalf("LoadOrderConfig failed: %v", err)
 	}
+	orderConfigs = append(orderConfigs, OrderRequest{
+		Dealer: "68db351696b56fcecafbc99",
+		Products: []Product{
+			{
+				ProductId: "68db479a96b56fcecafbc480",
+				Name:      "测试食物",
+				Quantity:  1,
+			},
+		},
+		Amount:  0,
+		PayType: "",
+	})
+	err = SaveOrderConfig(orderConfigs)
+	if err != nil {
+		t.Fatalf("SaveOrderConfig failed: %v", err)
+	}
+	t.Logf("SaveOrderConfig success! orderConfigs: %v", orderConfigs)
 
 }
